bfd962532721530555714ebb9a59776d78a7e818
CharsanStudios <125764739+CharsanStudios@users.noreply.github.com>
2023-02-27 15:21:26 -0300

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  $to = "seu-email@dominio.com";
+  $subject = "Formulário de Contato";
+  $body = "Nome: $name\nE-mail: $email\nMensagem:\n$message";
+
+  if (mail($to, $subject, $body)) {
+    echo "<p class='success'>Sua mensagem foi enviada com sucesso!</p>";
+  } else {
+    echo "<p class='error'>Houve um problema ao enviar a sua mensagem. Por favor, tente novamente.</p>";
+  }
+}
+?>
