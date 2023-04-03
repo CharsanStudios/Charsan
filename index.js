fb2f5083613cc7b52dd0c8af1518d23542cc433e
CharsanStudios <125764739+CharsanStudios@users.noreply.github.com>
2023-04-03 17:01:32 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ function exibirMensagem() {
     document.getElementById('mensagem').style.display = 'block'; // Adicionando esta linha
   }
 }
+
+  const image = document.querySelector("img"[1]),
+    input = document.querySelector("input");
+
+  input.addEventListener("change", () => {
+    image.src = URL.createObjectURL(input.files[0]);
+  });
+
+  const password = 'casjonata'; // defina sua senha aqui
+const redirectUrl = 'adms.html'; // defina a URL de redirecionamento aqui
+
+const form = document.querySelector('#login-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const passwordInput = document.querySelector('#password-input');
+  const enteredPassword = passwordInput.value;
+  if (enteredPassword === password) {
+    window.location.href = redirectUrl;
+  } else {
+    passwordInput.value = '';
+    alert('Senha incorreta. Tente novamente.');
+  }
+});
+
